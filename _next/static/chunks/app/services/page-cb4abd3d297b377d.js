15389c8d0d08aeefd6889c53c5785d6777f4a05a
Ashik <ashikshettyc@gmail.com>
2025-01-01 15:44:21 +0530

    about

---
+++ _next/static/chunks/app/services/page-cb4abd3d297b377d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7,286,763,851,974,977],{7403:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7970,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[970,441,517,358],()=>s(7403)),_N_E=e.O()}]);
